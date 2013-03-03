40989fab79b85ee77b0482593c2242b233837e38
Callum Stott <oetzi101@gmail.com>
2013-03-02 20:13:58 -0800

    Allow lifting contained type methods on Just and Nothing

--- lib/ruby-maybe.rb
+++ lib/ruby-maybe.rb
@@ -5,6 +5,18 @@ class Maybe
 end
 
 class Just < Maybe
+  def method_missing(method_name, *args, &block)
+    if Just.method_defined?(method_name)
+      super
+    else
+      values = args.map { |arg|
+        return Nothing.new if arg == Nothing.new
+        arg.kind_of?(Just) ? arg.value : arg
+      }
+      Just.new(@value.public_send(method_name, *values, &block))
+    end
+  end
+
   def initialize(value)
     @value = value
   end
@@ -31,6 +43,13 @@ class Just < Maybe
 end
 
 class Nothing < Maybe
+  def method_missing(method_name, *args, &block)
+    if Nothing.method_defined?(method_name)
+      super
+    else
+      Nothing.new
+    end
+  end
   def bind
     Nothing.new
   end
--- spec/ruby-maybe_spec.rb
+++ spec/ruby-maybe_spec.rb
@@ -7,6 +7,40 @@ describe "Just" do
       Just.new(5).bind { |val| Just.new(val * 2) }.must_equal Just.new(10)
     end
   end
+
+  describe "#==" do
+    it "returns false if the passed object is not a Just" do
+      (Just.new(5) == 5).must_equal false
+    end
+
+    it "returns true if the passed object is a Just with the same value" do
+      (Just.new(5) == Just.new(5)).must_equal true
+    end
+  end
+
+  describe "method lifting" do
+    it "allows using a method for the contained type" do
+      just = Just.new(5)
+      just.+(5).must_equal(Just.new(10))
+    end
+
+    it "allows passed arguments to be Maybe instances" do
+      just = Just.new(5)
+      just.+(Just.new(5)).must_equal(Just.new(10))
+    end
+
+    it "returns Nothing if any argument is Nothing" do
+      just = Just.new("Hello")
+      just.slice(Just.new(0), Nothing.new).must_equal(Nothing.new)
+    end
+
+    it "passes blocks correctly" do
+      count = 0
+      just = Just.new(5)
+      just.times { |i| count += i }
+      count.must_equal(10)
+    end
+  end
 end
 
 describe "Nothing" do
@@ -23,12 +57,18 @@ describe "Nothing" do
   end
 
   describe "#==" do
-    it "returns false if the passed object is not a Just" do
-      (Just.new(5) == 5).must_equal false
+    it "returns false if the passed object is not a Nothing" do
+      (Nothing.new == 5).must_equal false
     end
 
-    it "returns true if the passed object is a Just with the same value" do
-      (Just.new(5) == Just.new(5)).must_equal true
+    it "returns true if the passed object is a Nothing" do
+      (Nothing.new == Nothing.new).must_equal true
+    end
+  end
+
+  describe "method lifting" do
+    it "returns Nothing for any method call" do
+      Nothing.new.missing.must_equal(Nothing.new)
     end
   end
 end
@@ -43,16 +83,6 @@ describe "Maybe" do
 
     it "returns a Maybe" do
       Maybe.new.bind { |val| val }.kind_of?(Maybe).must_equal true
-    end
-  end
-
-  describe "#==" do
-    it "returns false if the passed object is not a Nothing" do
-      (Nothing.new == 5).must_equal false
-    end
-
-    it "returns true if the passed object is a Nothing" do
-      (Nothing.new == Nothing.new).must_equal true
     end
   end
 end
